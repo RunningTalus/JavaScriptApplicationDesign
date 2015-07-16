c9fb008c65b81a0d2d64761bdf34be368828630e
RunningTalus <dave@davemay.org>
2015-07-16 11:01:29 -0600

    Made changes to gruntfile, package.json, gitignore.

--- 03/01/Gruntfile.js
+++ 03/01/Gruntfile.js
@@ -1,23 +1,5 @@
 'use strict';
 
-<<<<<<< HEAD
-// the credentials we use to connect to our MySQL server
-var options = require('./db.json');
-
-module.exports = function(grunt){
-  grunt.initConfig({
-    db_create: { options: options },
-    db_upgrade: { options: options },
-    db_rollback: { options: options },
-    db_seed: { options: options }
-  });
-
-  // load all the tasks in the `tasks/` directory
-  grunt.loadTasks('./tasks');
-
-  // register a first time setup alias
-  grunt.registerTask('db_setup', 'Create, update, and seed a new database', ['db_create', 'db_upgrade', 'db_seed']);
-=======
 module.exports = function(grunt){
   grunt.initConfig({
     clean: {
@@ -111,5 +93,4 @@ module.exports = function(grunt){
   grunt.registerTask('build:release', 'Lint, compile, bundle, and optimize', [
     'clean', 'jshint', 'less:release', 'jade:release', 'concat:release', 'uglify:release'
   ]);
->>>>>>> origin/AppEnv
 };
